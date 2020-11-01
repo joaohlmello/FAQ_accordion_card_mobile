895a122771eeb1f99157ad31798c82c50a37b6bd
joaohlmello <73723829+joaohlmello@users.noreply.github.com>
2020-11-01 16:29:35 -0300

    initial commit

---
+++ scripts.js
@@ -0,0 +1,109 @@
+function openAnswer_1() {
+    document
+        .querySelector("#FAQ_1")
+        .classList
+        .toggle("on")
+
+    document
+        .querySelector("#FAQ_question_1")
+        .classList
+        .toggle("on")
+
+    document
+        .querySelector("#FAQ_arrow_1")
+        .classList
+        .toggle("on")
+
+    document
+        .querySelector("#FAQ_answer_1")
+        .classList
+        .toggle("on")
+}
+
+function openAnswer_2() {
+    document
+        .querySelector("#FAQ_2")
+        .classList
+        .toggle("on")
+
+    document
+        .querySelector("#FAQ_question_2")
+        .classList
+        .toggle("on")
+
+    document
+        .querySelector("#FAQ_arrow_2")
+        .classList
+        .toggle("on")
+
+    document
+        .querySelector("#FAQ_answer_2")
+        .classList
+        .toggle("on")
+}
+
+function openAnswer_3() {
+    document
+        .querySelector("#FAQ_3")
+        .classList
+        .toggle("on")
+
+    document
+        .querySelector("#FAQ_question_3")
+        .classList
+        .toggle("on")
+
+    document
+        .querySelector("#FAQ_arrow_3")
+        .classList
+        .toggle("on")
+
+    document
+        .querySelector("#FAQ_answer_3")
+        .classList
+        .toggle("on")
+}
+
+function openAnswer_5() {
+    document
+        .querySelector("#FAQ_5")
+        .classList
+        .toggle("on")
+
+    document
+        .querySelector("#FAQ_question_5")
+        .classList
+        .toggle("on")
+
+    document
+        .querySelector("#FAQ_arrow_5")
+        .classList
+        .toggle("on")
+
+    document
+        .querySelector("#FAQ_answer_5")
+        .classList
+        .toggle("on")
+}
+
+function openAnswer_4() {
+    document
+        .querySelector("#FAQ_4")
+        .classList
+        .toggle("on")
+
+    document
+        .querySelector("#FAQ_question_4")
+        .classList
+        .toggle("on")
+
+    document
+        .querySelector("#FAQ_arrow_4")
+        .classList
+        .toggle("on")
+
+    document
+        .querySelector("#FAQ_answer_4")
+        .classList
+        .toggle("on")
+}
